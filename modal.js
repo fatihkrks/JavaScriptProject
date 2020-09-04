7507cc63365839aa4991b3d0bef8ef09597b0d7d
Fatih Karakas <ygd03@vizyonturkey.com>
2020-09-04 17:20:00 +0300

    menu project add

---
+++ modal.js
@@ -0,0 +1,16 @@
+const modalBtn= document.querySelector(".modal-btn");
+
+
+const overlay=document.querySelector(".modal-overlay");
+
+
+
+const closeBtn=document.querySelector(".close-btn");
+
+
+modalBtn.addEventListener("click",function(){
+overlay.classList.add("open-modal");
+});
+closeBtn.addEventListener("click",function(){
+overlay.classList.remove("open-modal");
+});
